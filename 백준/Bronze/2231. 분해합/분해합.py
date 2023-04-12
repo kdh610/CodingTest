cdca31e0cc36f12185e1976c7a685fa7e9e89ef4
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-04-13 00:46:53 +0900

    [Bronze II] Title: 분해합, Time: 1760 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,18 @@
+
+
+n = int(input())
+answer = n
+
+for i in range(1,n+1):
+    result=i
+    m = str(i)
+    for j in m:
+        result += int(j)
+
+    if result == n:
+        answer = min(answer,i)
+
+if answer==n:
+    print(0)
+else:
+    print(answer)
