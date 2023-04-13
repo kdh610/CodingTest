e70180eccbe823984b4a6bac12dc89a432a7f95b
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-04-14 00:24:47 +0900

    [Gold IV] Title: 공유기 설치, Time: 840 ms, Memory: 38984 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,4 +1,5 @@
 import sys
+
 n, c = map(int,input().split())
 house = [int(sys.stdin.readline()) for _ in range(n)]
 house.sort()
@@ -9,21 +10,21 @@ while left<=right:
     mid = (left+right)//2
     cnt = 1
     cur = house[0]
-    check=False
+
     for i in range(1,len(house)):
         dis = house[i] -cur
-        if dis==mid:
-            check=True
         if dis < mid:
             continue
         else:
             cnt+=1
             cur = house[i]
+
     if cnt < c:
         right = mid-1
     elif cnt>=c:
         left = mid+1
         answer = max(answer, mid)
+
 
 
 
