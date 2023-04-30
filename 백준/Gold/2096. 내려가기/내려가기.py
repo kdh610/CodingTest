b0c08debc89e3fc4da36ed71a433eae9aceba756
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-04-30 22:49:43 +0900

    [Gold V] Title: 내려가기, Time: 436 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,30 +1,30 @@
 import sys
 
-input = sys.stdin.readline
-
 n = int(input())
 
-max_dp = [0] * 3
-min_dp = [0] * 3
 
-max_tmp = [0] * 3
-min_tmp = [0] * 3
+max_tmp = [0]*3
+min_tmp = [0]*3
+MAX = [0]*3
+MIN = [0]*3
+
+
 
 for i in range(n):
-    a, b, c = map(int, input().split())
+    a,b,c = map(int,sys.stdin.readline().split())
     for j in range(3):
-        if j == 0:
-            max_tmp[j] = a + max(max_dp[j], max_dp[j + 1])
-            min_tmp[j] = a + min(min_dp[j], min_dp[j + 1])
-        elif j == 1:
-            max_tmp[j] = b + max(max_dp[j - 1], max_dp[j], max_dp[j + 1])
-            min_tmp[j] = b + min(min_dp[j - 1], min_dp[j], min_dp[j + 1])
+        if j==0:
+            MAX[j] = a +  max(max_tmp[j], max_tmp[j+1])
+            MIN[j] = a + min(min_tmp[j], min_tmp[j + 1])
+        elif j==1:
+            MAX[j] = b + max(max_tmp[j],  max_tmp[j+1],  max_tmp[j-1])
+            MIN[j] = b + min(min_tmp[j], min_tmp[j + 1], min_tmp[j - 1])
         else:
-            max_tmp[j] = c + max(max_dp[j], max_dp[j - 1])
-            min_tmp[j] = c + min(min_dp[j], min_dp[j - 1])
+            MAX[j] = c + max(max_tmp[j],  max_tmp[j-1])
+            MIN[j] = c + min(min_tmp[j], min_tmp[j - 1])
 
-    for j in range(3):
-        max_dp[j] = max_tmp[j]
-        min_dp[j] = min_tmp[j]
+    for i in range(3):
+        max_tmp[i] = MAX[i]
+        min_tmp[i] = MIN[i]
 
-print(max(max_dp), min(min_dp))
+print(max(MAX),min(MIN))
