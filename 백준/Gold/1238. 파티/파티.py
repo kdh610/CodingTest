779be6bf5dbeccd128f9e9ab06270f7ed032aea6
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-05-02 22:41:17 +0900

    [Gold III] Title: 파티, Time: 1788 ms, Memory: 34348 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,49 @@
+import sys
+from heapq import *
+
+INF=int(1e9)
+
+n, m, x = map(int,sys.stdin.readline().split())
+
+graph=[[] for _ in range(n+1)]
+
+
+
+for i in range(m):
+    a,b,c = map(int,sys.stdin.readline().split())
+
+    graph[a].append((b,c))
+
+
+def dijkstra(start):
+    distance = [INF] * (n + 1)
+    q=[]
+    heappush(q,(0,start))
+    distance[start]=0
+
+    while q:
+        dist, now = heappop(q)
+
+        if dist > distance[now]:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if distance[i[0]] > cost:
+                distance[i[0]]=cost
+                heappush(q,(cost,i[0]))
+    return distance
+
+answer = 0
+
+for i in range(1,n+1):
+    depart = dijkstra(i)
+
+    arrival = dijkstra(x)
+
+    answer=max(answer,depart[x]+arrival[i])
+
+
+print(answer)
+
+
