6b2d44e9e05fc06da620cec39bd5b641fc54a6ad
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-05-20 16:39:48 +0900

    Update 암호 만들기.py

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -9,12 +9,14 @@ aeiou = ['a','e','i','o','u']
 moeum = []
 jaeum = []
 
+# 자음, 모음 분리
 for i in password:
     if i in aeiou:
         moeum.append(i)
     else:
         jaeum.append(i)
 
+# 문제 조건이 최소 모음 1개, 최소 자음 2개여서 조건 안맞으면 종료
 if len(jaeum)<2 or len(moeum)<1:
     exit()
 
@@ -23,6 +25,7 @@ jaeum.sort()
 
 
 for i in range(1,len(moeum)+1):
+    # 모음 2개이상 조합 가능한 경우만
     if L-i >=2:
         mo_comb = list(combinations(moeum,i))
         ja_comb = list(combinations(jaeum,L-i))
@@ -30,7 +33,9 @@ for i in range(1,len(moeum)+1):
         result =list(product(mo_comb,ja_comb))
 
         for i in result:
+            # result 결과가 튜플로 나와서 튜플을 합치고 정렬
             a,b = i
+            # 문자들 합쳐서 문자열로 만들기
             temp=''.join(sorted(a+b))
             answer.append(temp)
 
