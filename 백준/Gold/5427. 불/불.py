e5b404490c0e06cd89efb92bdac1abb65a1fe814
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-05-24 18:20:43 +0900

    [Gold IV] Title: 불, Time: 972 ms, Memory: 228852 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -6,29 +6,25 @@ dy = [0,0,1,-1]
 dx = [1,-1,0,0]
 
 for _ in range(T):
+    # 입력값 초기화
     w, h = map(int,input().split())
     building=[ list(sys.stdin.readline().strip()) for _ in range(h)]
 
     answer = []
-    visit = [[False] * w for _ in range(h)]
+    # visit = [[False] * w for _ in range(h)]
     fire=[]
+    Q = deque()
 
     for i in range(h):
         for j in range(w):
             if building[i][j]=='*':
-                fire.append((i,j))
+                Q.append((i,j))
+                # visit[i][j]=True
             elif building[i][j]=='@':
                 cur = (i,j)
                 building[i][j]=0
 
-
-    Q = deque()
-    Q.append(cur)
-
-
-    for a,b in fire:
-        Q.append((a,b))
-        visit[a][b]=True
+    Q.appendleft(cur)
 
     while Q:
         y,x = Q.popleft()
@@ -37,14 +33,14 @@ for _ in range(T):
             ny = y + dy[i]
             nx = x + dx[i]
 
-
             if 0<=ny<h and 0<=nx<w:
+                # 사람일때
                 if isinstance(building[y][x], int) and building[ny][nx] == '.':
                     building[ny][nx] = building[y][x] + 1
                     Q.append((ny, nx))
-
-                elif building[y][x]=='*' and building[ny][nx]!='#' and not visit[ny][nx]:
-                    visit[ny][nx]=True
+                # 불일때
+                elif building[y][x]=='*' and building[ny][nx]!='#' and  building[ny][nx]!='*':
+                    # visit[ny][nx]=True
                     building[ny][nx] = '*'
                     Q.append((ny,nx))
             else:
