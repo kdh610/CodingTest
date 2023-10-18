245e0ab867fa0f22382e9b49bbddeaa600627f80
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-10-19 00:14:33 +0900

    [Gold V] Title: 수 고르기, Time: 180 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,31 +1,29 @@
 import sys
-
 n,m = map(int,input().split())
 
 arr=[int(sys.stdin.readline()) for _ in range(n)]
 
+
+
 arr.sort()
-
-dif=-1
-answer = int(2e9)+1
-end = 0
-
-for i in range(n):
-    if end==n:
-        end-=1
-    if end<n:
-        dif = arr[end] - arr[i]
-    while dif<m and end<n:
-        dif = arr[end] - arr[i]
-        end+=1
+left, right = 0,0
 
 
-    if dif>=m:
-        answer = min(answer,dif)
+ans = int(2e9)+1
 
+while left<=right and right<=n:
 
+    dif = arr[right] - arr[left]
 
+    if dif<m:
+        right+=1
+        if right>=n:
+            break
+    else:
+        left+=1
+        ans = min(ans,dif)
 
-
-print(answer)
-
+if m ==0:
+    print(0)
+else:
+    print(ans)
