eb70e5e2cdef20a9ee3f6fcb4159037c96785570
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-10-18 19:59:48 +0900

    [Silver IV] Title: 수들의 합 2, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,30 +1,34 @@
 
 
 n,m = map(int,input().split())
+
 arr = list(map(int,input().split()))
 
-left, right = 0,1
-sum = arr[0]
-ans = 0
+left, right =0,1
 
-while left<=right and right<=n:
-    if sum == m:
-        ans+=1
+sum = arr[left]
+cnt = 0
 
-        sum-=arr[left]
-        left += 1
+while left<= right and right<=n:
+
+    if sum==m:
+        cnt+=1
+        sum -= arr[left]
+        left+=1
     elif sum < m:
-        
-        if right==n:
+        if right>=n:
             break
-        sum+=arr[right]
-        right += 1
-    elif sum > m:
+        sum += arr[right]
+        right +=1
+
+    elif sum>m:
         sum -= arr[left]
         left +=1
 
+print(cnt)
 
-print(ans)
+
+
 
 
 
