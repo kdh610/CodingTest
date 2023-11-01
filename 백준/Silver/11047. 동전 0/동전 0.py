b0aa6e17909905f76f382077982c306842c48016
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-11-01 19:22:05 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,15 +1,21 @@
-
 import sys
-n,k = map(int,input().split())
 
-coin = [ int(input()) for _ in range(n)]
 
-coin.sort(reverse=True)
+n,k=map(int,input().split())
+
+coins=[int(input()) for _ in range(n)]
+
+coins.sort(reverse=True)
+
 answer=0
-for c in coin:
-    answer += k // c
-    k=k%c
 
+for coin in coins:
 
+    if k>=coin:
+        answer+=k//coin
+        k-=k//coin*coin
+
+    if k<=0:
+        break
 
 print(answer)
