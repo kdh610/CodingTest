d56bf4288517db471e00078a70cb562f4193ea70
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2023-11-10 22:04:24 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,32 @@
+import sys
+
+
+n,m = map(int,input().split())
+
+board = [sys.stdin.readline().strip() for _ in range(n)]
+answer=64
+for i in range(n):
+    for j in range(m):
+
+        if i+7 <=n-1 and j+7<=m-1:
+
+            tmp = [board[k][j:j+8] for k in range(i,i+8)]
+            cnt=0
+
+            for r in range(8):
+                for c in range(8):
+                    if r%2==0 or r==0:
+                        if (c%2==0 or c==0) and tmp[r][c] == 'W':
+                            cnt += 1
+                        if (c%2==1 ) and tmp[r][c]=='B':
+                            cnt+=1
+                    else:
+
+                        if (c%2==0 or c==0) and tmp[r][c] == 'B':
+                            cnt += 1
+                        if (c%2==1) and tmp[r][c]=='W':
+                            cnt+=1
+
+            answer=min(answer,cnt,64-cnt)
+
+print(answer)
