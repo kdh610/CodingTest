4fac8191e29d2684676244c37c316acb0155659d
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2024-02-14 00:00:28 +0900

    [Gold IV] Title: 단어 수학, Time: 6588 ms, Memory: 111568 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -18,19 +18,15 @@ for i in range(N):
 
 
 alpha = list(alpha)
-dict = dict()
+
 answer = 0
 for perm in permutations(numbers,len(alpha)):
     new_words =words
     result=0
-    #print(perm)
     for i in range(len(alpha)):
-        #print(alpha[i],perm[i])
         new_words = new_words.replace(alpha[i],perm[i])
-        #print(new_words)
     for i in new_words.split('+'):
         if i.isdigit():
-            #print(i)
             result+=int(i)
 
     answer=max(answer, result)
