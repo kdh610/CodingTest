99fb3315f71bf1159b2629e0c76ff941973a7789
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2025-01-13 17:30:12 +0900

    [level 2] Title: 더 맵게, Time: 1699.08 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,43 @@
+import java.util.PriorityQueue;
+
+public class Solution {
+	
+    public static int solution(int[] scoville, int K) {
+        int answer = -1;
+        
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        
+        for(int i : scoville) {
+        	heap.offer(i);
+        }
+        
+        int cnt=0;
+        while(true) {
+     	if(heap.size()==1) {
+        		if(heap.poll()<K) {
+        			cnt=-1;
+        		}
+        		
+        		break;
+        	}
+        	
+        	int first =heap.poll();
+        	int second = heap.poll();
+        	
+        	if(first>=K) {
+        		break;
+        	}
+        	
+        	int s = first + (second*2);
+        	cnt++;
+        	heap.offer(s);
+        	
+        	
+        }
+        
+        answer = cnt;
+        
+        return answer;
+    }
+}
