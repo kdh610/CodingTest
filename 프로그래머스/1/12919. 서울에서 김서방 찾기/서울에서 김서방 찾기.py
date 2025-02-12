06e9cc65e3f3a3d09ff6aa20c385744fd11d6f44
DoHoon Kim <68257796+vpfl95@users.noreply.github.com>
2025-02-12 19:27:23 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -0,0 +1,9 @@
+def solution(seoul):
+    answer = ''
+    
+    for i,v in enumerate(seoul):
+        if v=="Kim":
+            answer="김서방은 "+str(i)+"에 있다"
+            break
+    
+    return answer
