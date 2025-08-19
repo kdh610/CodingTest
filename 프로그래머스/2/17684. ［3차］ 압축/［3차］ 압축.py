b4135e0aeab1f8774449cce2fb84c72ddf6553f2
Dohoon Kim <68257796+kdh610@users.noreply.github.com>
2025-08-19 21:58:23 +0900

    [level 2] Title: [3차] 압축, Time: 0.56 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,27 +1,32 @@
 def solution(msg):
+    dict = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10,'K':11,'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
+    last=26
+    length = 0
     answer = []
-    index = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13,
-             'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25,
-             'Z': 26}
+    start = 0
+    end = 1
 
-    start=0
-    end=1
-    last=27
-    result=''
+    while start<len(msg):
+        idx=0
 
-    while end < len(msg)+1:
-        w = msg[start:end]
+        while True:
+            w = msg[start:end]
+            if w=='':
+                break
+            if w not in dict:
+                last+=1
+                dict[w]=last
 
+                break
 
-        if w in index:
-            result =w
+            idx = dict[w]
+
             end+=1
-        else:
-            index[w] = last
-            last+=1
-            start=end-1
-            end=start+1
-            answer.append(index[result])
-    answer.append(index[result])
+            if end>len(msg):
+                break
 
+        if idx!=0:
+            answer.append(idx)
+
+        start = end-1
     return answer
